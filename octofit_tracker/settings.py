aa8b84bb10be53615f502afba0637d47f9df1fa2
valarg08 <g.valar.2008@gmail.com>
2025-04-10 17:45:04 +0000

    Update project settings and configurations

--- octofit_tracker/settings.py
+++ octofit_tracker/settings.py
@@ -79,17 +79,11 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
     }
 }
-
-db_from_env = {
-    "ENGINE": "djongo",
-    "NAME": "octofit_db",
-}
-DATABASES["default"].update(db_from_env)
 
 
 # Password validation
